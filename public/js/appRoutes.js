de7b142c28572009399bc9d2df7a525b0d3d8b74
Tina Jain <tjain@spiderlogic.com>
2015-10-31 15:53:52 +0530

    Finally routing works now!

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,25 +1,20 @@
-angular.module('appRoutes', []).config(['$stateProvider', '$urlRouteProvider', function($stateProvider, $urlRouteProvider) {
+angular.module('appRoutes', []).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
-
 		.state('home', {
 			url: '/',
-			templateUrl: 'views/home.html',
-			controller: 'MainController'
+			templateUrl: 'views/home.html'
 		})
-
 		.state('nerds', {
 			url: '/nerds',
 			templateUrl: 'views/nerd.html',
 			controller: 'NerdController'
 		})
-
 		.state('geeks', {
 			url: '/geeks',
 			templateUrl: 'views/geek.html',
 			controller: 'GeekController'
 		})
 
-	$urlRouteProvider.otherwise('/');
-
+	$urlRouterProvider.otherwise('/');
 }]);
